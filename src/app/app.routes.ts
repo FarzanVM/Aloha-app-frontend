814412c53faef346f26d77cd697442e282d4327c
Farzan V V M <farzan.61093361@ltimindtree.com>
2025-04-03 15:51:21 +0530

    commiting for version 17

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { LoginComponent } from './homepage/login/login.component';
+import { SignupComponent } from './homepage/signup/signup.component';
+import { ChatuiComponent } from './chatui/chatui.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component:HomepageComponent,
+        children:[
+            {
+                path:'',
+                component:LoginComponent
+            },
+            {
+                path:'signup',
+                component:SignupComponent
+            }
+        ]
+    },
+    {
+        path:'chatapp',
+        component:ChatuiComponent
+    }
+];
